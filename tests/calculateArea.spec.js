20cb3303d3b240e06f150163a75ea337c95b96b1
Sergio Otero <sergotero@gmail.com>
2025-10-19 18:06:12 +0200

    fix: bug solved

--- tests/calculateArea.spec.js
+++ tests/calculateArea.spec.js
@@ -12,26 +12,26 @@ describe("Iteration 3", () => {
             expect(calculateArea.length).toBe(2);
         });
 
-        it("the function should return a number representing the area of a rectangle (the product of the two arguments", () => {
+        it("the function should return a number representing the area of a rectangle (the product of the two arguments)", () => {
             expect(calculateArea(2, 2)).toEqual(4);
             expect(calculateArea(3, 5)).toEqual(15);
             expect(calculateArea(7, 7)).toEqual(49);
             expect(calculateArea(9, 8)).toEqual(72);
         });
 
-        it("in case any of the arguments is not provided, the function should return undefined"), () => {
+        it("in case any of the arguments is not provided, the function should return undefined", () => {
             expect(calculateArea()).toEqual(undefined);
             expect(calculateArea(1)).toEqual(undefined);
             expect(calculateArea(undefined, 20)).toEqual(undefined);
             expect(calculateArea(15, undefined)).toEqual(undefined);
             expect(calculateArea(undefined, undefined)).toEqual(undefined);
-        }
+        });
 
         it("should return undefined if any of the two arguments is not a number", () =>{
             expect(add("a", 1)).toEqual(undefined);
             expect(add(1, "a")).toEqual(undefined);
             expect(add("b", "a")).toEqual(undefined);
-        })
+        });
 
-    })    
-})
+    });
+});
