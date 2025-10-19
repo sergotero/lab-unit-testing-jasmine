8da59f79912bcf1c963f8cecdc6d0ba0636ea54e
Sergio Otero <sergotero@gmail.com>
2025-10-19 17:02:57 +0200

    feat: iteration 01 completed

--- src/add.js
+++ src/add.js
@@ -1,7 +1,11 @@
 function add (numOne, numTwo) {
-    if (numOne == undefined || numTwo == undefined) {
+    if(typeof numOne !== "number" ||
+        typeof numTwo !== "number" || 
+        numOne == undefined || 
+        numTwo == undefined)
+    {
         return undefined;
-    }
+    } 
 
     return numOne + numTwo;
 }
